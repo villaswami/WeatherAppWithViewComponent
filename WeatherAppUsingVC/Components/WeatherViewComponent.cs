b073ee4af3bb4ba06b8683165325994447de45d8
villaswami <88075285+villaswami@users.noreply.github.com>
2024-08-23 22:21:16 +0530

    Add files via upload

---
+++ WeatherAppUsingVC/Components/WeatherViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WeatherAppUsingVC.Models;
+
+namespace WeatherAppUsingVC.Components
+{
+    public class WeatherViewComponent:ViewComponent
+    {
+        public async  Task<IViewComponentResult> InvokeAsync(CityWeather city)
+        {
+            ViewBag.CssStyle = GetCssClassByFahrenheit(city.TemperatureFahrenheit);
+            return View(city);
+
+        }
+       private string GetCssClassByFahrenheit(int TemperatureFahrenheit)
+        {
+            return TemperatureFahrenheit switch
+            {
+                (< 44) => "blue-back",
+                (>= 44) and (< 75) => "green-back",
+                (>= 75) => "orange-back"
+            };
+        }
+    }
+}
